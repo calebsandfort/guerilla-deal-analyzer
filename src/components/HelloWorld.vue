d7045bc9cfdbdb54f5d96555bfe7f5ff8a8c56a1
Caleb Sandfort <csandfort@isupport.com>
2019-03-21 13:41:01 -0700

    add logo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
     >
       <v-flex xs12>
         <v-img
-          :src="require('../assets/logo.svg')"
+          :src="require('../assets/guerillaLogo.png')"
           class="my-3"
           contain
           height="200"
@@ -15,12 +15,10 @@
 
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Let's Make a Deal!
         </h1>
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+          Various tools for anaylzing real estate investement leads.
         </p>
       </v-flex>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import apolloProvider from './apollo'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
