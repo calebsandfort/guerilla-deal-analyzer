139f8c6cd84858b31c043a446583e3e0e41d7ab3
Ubuntu <caleb.sandfort@gmail.com>
2019-04-29 22:02:25 +0000

    deploy work

--- src/backend/services/scrape/zillow-scraper.spec.js
+++ src/backend/services/scrape/zillow-scraper.spec.js
@@ -22,17 +22,17 @@ describe("zillow-scraper", () => {
   //     expect(response.length).to.eql(expectedResult);
   //   });
   // });
-  // it("it finds the zillow url for the given address", function() {
-  //   this.timeout(10 * 60 * 1000);
-  //
-  //   const expectedResult = ZILLOW_PROPERTY_URL;
-  //
-  //   return zillowScraper
-  //     .findZillowUrl("124 SW Woods St")
-  //     .then(function(response) {
-  //       expect(response).to.eql(expectedResult);
-  //     });
-  // });
+  it("it finds the zillow url for the given address", function() {
+     this.timeout(10 * 60 * 1000);
+  
+     const expectedResult = ZILLOW_PROPERTY_URL;
+  
+     return zillowScraper
+       .findZillowUrl("124 SW Woods St")
+       .then(function(response) {
+         expect(response).to.eql(expectedResult);
+       });
+   });
   // it("finds a property for the given address", function() {
   //   this.timeout(10 * 60 * 1000);
   //
@@ -45,16 +45,16 @@ describe("zillow-scraper", () => {
   //     });
   // });
   //
-  // it("finds a property for the given address with no city/state", function() {
-  //   this.timeout(10 * 60 * 1000);
-  //
-  //   const expectedResult = getSimpleExpectedProperty();
-  //
-  //   return zillowScraper.findProperty(ADDRESS).then(function(response) {
-  //     expect(castToSimpleProperty(response)).to.eql(expectedResult);
-  //   });
-  // });
-  //
+   it("finds a property for the given address with no city/state", function() {
+     this.timeout(10 * 60 * 1000);
+  
+     const expectedResult = getSimpleExpectedProperty();
+  
+     return zillowScraper.findProperty(ADDRESS).then(function(response) {
+       expect(castToSimpleProperty(response)).to.eql(expectedResult);
+     });
+   });
+  
   // it("finds a property for the given url", function() {
   //   this.timeout(10 * 60 * 1000);
   //
@@ -116,19 +116,19 @@ describe("zillow-scraper", () => {
   //         .to.eql(expectedResult);
   //     });
   // });
-  it("scrapes property tax info", function() {
-    this.timeout(10 * 60 * 1000);
-
-    const expectedResult = {};
-
-    return zillowScraper
-      .findPropertyTaxInfo({
-        streetAddress: ADDRESS
-      })
-      .then(function(response) {
-        expect(response).to.eql(expectedResult);
-      });
-  });
+ // it("scrapes property tax info", function() {
+   // this.timeout(10 * 60 * 1000);
+//
+  //  const expectedResult = {};
+//
+  //  return zillowScraper
+    //  .findPropertyTaxInfo({
+   //     streetAddress: ADDRESS
+   //   })
+   //   .then(function(response) {
+  //      expect(response).to.eql(expectedResult);
+    //  });
+ // });
   // it("trial scrapes", function() {
   //   this.timeout(10 * 60 * 1000);
   //
