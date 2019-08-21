039adaa05f4fce26eddf7c5b6548fe70a51aa7f0
Caleb Sandfort <csandfort@isupport.com>
2019-08-21 13:55:41 -0700

    update for new server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "ec2-34-222-11-55.us-west-2.compute.amazonaws.com",
-      key: "~/.ssh/cspdealtools.pem",
+      host: "ec2-52-38-254-207.us-west-2.compute.amazonaws.com",
+      key: "~/.ssh/cspdealtools2.pem",
       ref: "origin/master",
       repo: "git@github.com:calebsandfort/guerilla-deal-analyzer.git",
       path: "/home/ubuntu/cspdealtools",
